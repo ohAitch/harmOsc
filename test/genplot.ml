234ca0555430318c991d441045e63b59398406e8
Anton Dyudin <ohaitch@lemnos.ashima>
2012-11-14 20:55:31 -0800

    unified

--- test/genplot.ml
+++ test/genplot.ml
@@ -15,12 +15,16 @@ let reversed, _, _ = List.fold_left (
       if sign *. x < 0. || x = 0. then ((e, i)::acc, x, 1.)
         else ((e, nan)::acc, x, i +. 1.)) ([], 0., 1.) vals
 in List.rev reversed;;
-for iter = 1 to 5 do
-  for jter = 1 to 5 do
-    let k = 10.**((float iter) /. 3. +. 3.) and mu =  -120. /. (float jter) and pts = 10000 in
-      let out = open_out(sprintf "Sim%d-%d" iter jter) in
+let out = open_out("Surface") in
+fprintf out "Springiness Friction log(scale)";
+for iter = 1 to 50 do
+  for jter = 1 to 50 do
+    let k = 10.**((float iter) /. 30. +. 3.) and mu =  -1200. /. (float jter) and pts = 10000 in
+      (*let out = open_out(sprintf "Sim%d-%d" iter jter) in
         fprintf out "#k = %g, μ = %g\n" k mu;
         fprintf out "Energy Frequency\n";
-          List.iter (fun (e, p) -> fprintf out "%f %f\n" e (float pts /. p))
-            (e_and_p ~k:k (model ~k:k ~mu:mu ~points:pts 10.)); close_out out
+          List.iter (fun (e, p) -> fprintf out "%f %f\n" e (float pts /. p))*)
+           let l = (e_and_p ~k:k (model ~k:k ~mu:mu ~points:pts 10.)) in
+             let (init, _), (fin, _) = List.hd l, List.hd (List.rev l) in
+               fprintf out "%f %f %f\n" k mu (log (init /. fin))
 done done;;
