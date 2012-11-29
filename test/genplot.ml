8967aac808cbb61ca746abf6a8c6bce6b07db6b8
Anton Dyudin <ohAitch@gmail.com>
2012-11-29 13:38:17 -0800

    Narrowed mu range.

--- test/genplot.ml
+++ test/genplot.ml
@@ -19,7 +19,7 @@ let energy ?(k = 1.) (x, v, _) = k*.x**2. +. v**2.;;
 let out = open_out("Surface") in
 for iter = 1 to 50 do
   for jter = 1 to 50 do
-    let k = 1000. *. (float iter) and mu =  -24. *. (float jter) and delta, pts = 1e-3, 10000 in
+    let k = 1000. *. (float iter) and mu =  -800. -. 8. *. (float jter) and delta, pts = 1e-3, 10000 in
       let l = (model ~k:k ~mu:mu ~delta:delta ~points:pts 10.) in
          let init, fin = energy ~k:k (List.hd l), energy ~k:k (List.hd (List.rev l)) in (*Extract initial/final energy*)
             let growth = init /. fin and frequency = float (List.length (period l)) /. (float pts *. delta) in 
