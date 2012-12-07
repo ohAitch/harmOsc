65819b960b8e5e8896f4dfa3dcee44954b59ef64
Anton Dyudin <ohAitch@gmail.com>
2012-12-06 17:01:50 -0800

    Switched to looking at mu

--- test/genplot.ml
+++ test/genplot.ml
@@ -4,24 +4,24 @@ open Printf;;
 let repeat n f x =
   let rec make_list n f l = if n == 0 then l else make_list (n-1) f ((f (List.hd l))::l) in
     List.rev (make_list n f x);; (*Still looking for a primitve*) 
-let model ?(delta = 1e-4) ?(z0 = 0.) ?(k = 1.) ?(mu = -. 1e-3) ?(points = 50000) init_val = 
+let model ?(delta = 1e-4) ?(z0 = 0.) ?(k = 1.) ?(mu = -. 1e-3) ?(points = 50000) ?(m = 1.) init_val = 
   let t_prime (z, v, a) = 
-    (z +. delta *. v , v +. delta *. a, mu *. v -. ((z -. z0) *. k) )
+    (z +. delta *. v , v +. delta *. a, mu *. v -. ((z -. z0) *. k /. m) )
   in repeat points t_prime [init_val, delta, 0.];;
 let period vals =
-let reversed, _, _ = List.fold_left (
+let intervals, _, _ = List.fold_left (
   fun (acc, sign, i) (_, v, _) ->
-      if sign *. v < 0. then (i::acc, v, 1.)
-        else (acc, v, i +. 1.))
-  ([], 0., 1.) vals
-in List.rev reversed;;
-let energy ?(k = 1.) (x, v, _) = k*.x**2. +. v**2.;; 
+      if sign *. v < 0. then (i::acc, v, 1.) else (acc, v, i +. 1.))
+  ([], nan, 0.) vals
+and avg l = (List.fold_left ( +. ) 0. l) /. float (List.length l)
+in avg intervals ;;
+let energy ?(k = 1.) ?(m = 1.) (x, v, _) = 0.5 *. (k*.x**2. +. m*.v**2.);; 
 let out = open_out("Surface") in
-for iter = 1 to 250 do
-    let k = 10. *. (float iter) and mu =  -5. and time, pts = 10., 10000 in
-      let l = (model ~k:k ~mu:mu ~delta:(time /. float pts) ~points:pts 10.) in
+for iter = 1 to 25 do
+    let k = 1. and mu = (float iter) *. -0.01 and delta, pts = 1e-4, 100000 in
+      let l = (model ~k:k ~mu:mu ~delta:delta ~points:pts 10.) in
          let init, fin = energy ~k:k (List.hd l), energy ~k:k (List.hd (List.rev l)) in (*Extract initial/final energy*)
-            let growth = init /. fin and
-                frequency = float (List.length (period l)) /. time in 
-               fprintf out "%f %f %f %f\n" k mu ((log growth) /. k) frequency
+            let growth = fin /. init and
+                frequency = (period l)** -1. /. delta in 
+               fprintf out "%f %f %f %f\n" k mu growth (frequency /. (k ** 0.5))
 done ;;
